c18854d9fba6840833dab69d998a96f0023e128a
Håkon Jacobsen <jacobsen.haakon@gmail.com>
2011-09-03 16:51:03 +0200

    Set up a test framework to run the provided vectors of each hash algorothm

--- Tests/testframe.hs
+++ Tests/testframe.hs
@@ -1,23 +1,48 @@
 import Test.HUnit
 import qualified Data.ByteString.Lazy as L
 import Text.ParserCombinators.Parsec
-import Data.Binary
-import Control.Monad (liftM2, liftM3)
-import Control.Applicative ((<$))
+import Data.Binary (encode)
+import Control.Applicative ((*>),(<*),(<*>),(<$>),liftA3,liftA)
+import System.IO
 
-test1 :: Test 
-test1 = TestCase (assertEqual "input1" (00 ::Integer) (0xF2E180FB5947BE964CD584E22E496242C6A329C577FC4CE8C36D34C3))
+hashFunc :: L.ByteString -> L.ByteString
+hashFunc _ = encode "0xF2E180FB5947BE964CD584E22E49624"
 
-tests = TestList [TestLabel "JH ShortMsgKAT_224" test1]
+tf2Test :: TestFile -> Test
+tf2Test tf = let fHeader = header tf
+             in TestLabel ("File: " ++ (fName fHeader) ++
+                           "Algorithm: " ++ (algName fHeader))
+                          (tf2TestList tf)
+
+tf2TestList :: TestFile -> Test
+tf2TestList = TestList . map ti2TestCase . testInstances
+
+ti2TestCase :: TestInstance -> Test
+ti2TestCase ti = TestCase $ 
+   assertEqual (show $ len ti) (digest ti) (hashFunc $ msg ti)
+
+
+
+----------------------------- Parse test vector files --------------------------
+
+main :: IO ()
+main = do 
+   file <- readFile "./testvectors/jh/KAT_MCT/ShortMsgKAT_224.txt"
+   let p_result = parse p_testFile "ShortMsgKAT_224.txt" file
+   case p_result of  
+      Left parseError -> putStrLn "FAIL"
+      Right result -> do runTestTT $ tf2TestList result
+                         putStrLn "SUCCESS!"
+   
 
 
 data TestFile = TestFile {
          header :: Header,
-         testsInstances :: [TestInstance]
-      }
+         testInstances :: [TestInstance]
+      } deriving (Show)
 
 data Header = Header {
-         testFileName :: String,
+         fName :: String,
          algName :: String
       } deriving (Show)
 
@@ -27,23 +52,25 @@ data TestInstance = TI {
          digest :: L.ByteString
       } deriving (Show)
       
-testFile :: GenParser Char st TestFile
-testFile = liftM2 TestFile (parseHeader) (sepBy testInstance eol)
+p_testFile :: GenParser Char st TestFile
+p_testFile = TestFile <$> 
+   (p_header <* newline) <*> (sepEndBy p_testInstance newline)
 
-parseHeader :: GenParser Char st Header
-parseHeader = do
-   tfn <- between (string "# ") newline $ manyTill anyChar (string ".txt")
-   alg <- between (string "# Algorithm Name: ") newline $ many (noneOf "\n")
-   skipMany1 (manyTill anyChar newline)
+p_header :: GenParser Char st Header
+p_header = do
+   tfn <- string "# " *> manyTill anyChar (string ".txt\n")
+   alg <- string "# Algorithm Name: " *> manyTill anyChar newline
+   manyTill anyChar newline
    return $ Header tfn alg
 
-testInstance :: GenParser Char st TestInstance
-testInstance = liftM3 TI len msg digest
-   where len = undefined --string "Len = " *> many digit
-         msg = undefined --string "Msg = " *> manyTill 
-         digest = undefined
-      
-eol = count 2 newline
+p_testInstance :: GenParser Char st TestInstance
+p_testInstance = liftA3 TI len msg digest
+   where len = f "Len = " read digit
+         msg = f "Msg = " encode hexDigit
+         digest = f "MD = " encode hexDigit
+         f prefix reader format = 
+            reader <$> (string prefix *> manyTill format newline)
+
       
       
       
